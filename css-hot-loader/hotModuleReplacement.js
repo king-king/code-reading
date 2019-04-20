ad9e072d6839452747d0aa5f7820bd09fbc3b87c
wangqun6 <wangqun28>
2019-04-20 09:49:59 +0800

    format

--- css-hot-loader/hotModuleReplacement.js
+++ css-hot-loader/hotModuleReplacement.js
@@ -7,123 +7,127 @@ var forEach = Array.prototype.forEach;
 
 var noop = function () {};
 
-var getCurrentScriptUrl = function(moduleId) {
-  var src = srcByModuleId[moduleId];
+var getCurrentScriptUrl = function (moduleId) {
+    var src = srcByModuleId[moduleId];
 
-  if (!src) {
-    if (document.currentScript) {
-      src = document.currentScript.src;
-    } else {
-      var scripts = document.getElementsByTagName('script');
-      var lastScriptTag = scripts[scripts.length - 1];
+    if (!src) {
+        if (document.currentScript) {
+            src = document.currentScript.src;
+        } else {
+            var scripts = document.getElementsByTagName('script');
+            var lastScriptTag = scripts[scripts.length - 1];
 
-      if (lastScriptTag) {
-        src = lastScriptTag.src;
-      }
+            if (lastScriptTag) {
+                src = lastScriptTag.src;
+            }
+        }
+        srcByModuleId[moduleId] = src;
     }
-    srcByModuleId[moduleId] = src;
-  }
 
-  return function(fileMap) {
-    var splitResult = /([^\\/]+)\.js$/.exec(src);
-    var filename = splitResult && splitResult[1];
-    if (!filename) {
-      return [src.replace('.js', '.css')];
-    }
-    return fileMap.split(',').map(function(mapRule) {
-      var reg = new RegExp(filename + '\\.js$', 'g')
-      return normalizeUrl(src.replace(reg, mapRule.replace(/{fileName}/g, filename) + '.css'), { stripWWW: false });
-    });
-  };
+    return function (fileMap) {
+        var splitResult = /([^\\/]+)\.js$/.exec(src);
+        var filename = splitResult && splitResult[1];
+        if (!filename) {
+            return [src.replace('.js', '.css')];
+        }
+        return fileMap.split(',').map(function (mapRule) {
+            var reg = new RegExp(filename + '\\.js$', 'g')
+            return normalizeUrl(src.replace(reg, mapRule.replace(/{fileName}/g, filename) + '.css'), {
+                stripWWW: false
+            });
+        });
+    };
 };
 
 function updateCss(el, url) {
-  if (!url) {
-    url = el.href.split('?')[0];
-  }
-  if (el.isLoaded === false) {
-    // We seem to be about to replace a css link that hasn't loaded yet.
-    // We're probably changing the same file more than once.
-    return;
-  }
-  if (!url || !(url.indexOf('.css') > -1)) return;
-
-  el.visited = true;
-  var newEl = el.cloneNode();
-
-  newEl.isLoaded = false;
-  newEl.addEventListener('load', function () {
-    newEl.isLoaded = true;
-    newEl.parentNode.removeChild(el);
-  });
-
-  newEl.addEventListener('error', function () {
-    newEl.isLoaded = true;
-    if (newEl.parentNode.contains(el)) {
-      newEl.parentNode.removeChild(el);
+    if (!url) {
+        url = el.href.split('?')[0];
     }
-  });
+    if (el.isLoaded === false) {
+        // We seem to be about to replace a css link that hasn't loaded yet.
+        // We're probably changing the same file more than once.
+        return;
+    }
+    if (!url || !(url.indexOf('.css') > -1)) return;
 
-  newEl.href = url + '?' + Date.now();
-  // insert new <link /> right to the old one's position
-  el.parentNode.insertBefore(newEl, el.nextSibling);
+    el.visited = true;
+    var newEl = el.cloneNode();
+
+    newEl.isLoaded = false;
+    newEl.addEventListener('load', function () {
+        newEl.isLoaded = true;
+        newEl.parentNode.removeChild(el);
+    });
+
+    newEl.addEventListener('error', function () {
+        newEl.isLoaded = true;
+        if (newEl.parentNode.contains(el)) {
+            newEl.parentNode.removeChild(el);
+        }
+    });
+
+    newEl.href = url + '?' + Date.now();
+    // insert new <link /> right to the old one's position
+    el.parentNode.insertBefore(newEl, el.nextSibling);
 }
 
 function reloadStyle(src) {
-  var elements = document.querySelectorAll('link');
-  var loaded = false;
+    var elements = document.querySelectorAll('link');
+    var loaded = false;
 
-  forEach.call(elements, function(el) {
-    if (el.visited === true) return;
+    forEach.call(elements, function (el) {
+        if (el.visited === true) return;
 
-    var url = getReloadUrl(el.href, src);
-    if (url) {
-      updateCss(el, url);
-      loaded = true;
-    }
-  });
+        var url = getReloadUrl(el.href, src);
+        if (url) {
+            updateCss(el, url);
+            loaded = true;
+        }
+    });
 
-  return loaded;
+    return loaded;
 }
 
 function getReloadUrl(href, src) {
-  href = normalizeUrl(href, { stripWWW: false });
-  var ret;
-  src.some(function(url) {
-    if (href.indexOf(src) > -1) {
-      ret = url;
-    }
-  });
-  return ret;
+    href = normalizeUrl(href, {
+        stripWWW: false
+    });
+    var ret;
+    src.some(function (url) {
+        if (href.indexOf(src) > -1) {
+            ret = url;
+        }
+    });
+    return ret;
 }
 
 function reloadAll() {
-  var elements = document.querySelectorAll('link');
-  forEach.call(elements, function(el) {
-    if (el.visited === true) return;
-    updateCss(el);
-  });
+    var elements = document.querySelectorAll('link');
+    forEach.call(elements, function (el) {
+        if (el.visited === true) return;
+        updateCss(el);
+    });
 }
 
-module.exports = function(moduleId, options) {
-  var getScriptSrc;
+module.exports = function (moduleId, options) {
+    var getScriptSrc;
 
-  if (noDocument) {
-    return noop;
-  }
-
-  getScriptSrc = getCurrentScriptUrl(moduleId);
-
-  function update() {
-    var src = getScriptSrc(options.fileMap);
-    var reloaded = reloadStyle(src);
-    if (reloaded && !options.reloadAll) {
-      console.log('[HMR] css reload %s', src.join(' '));
-    } else {
-      console.log('[HMR] Reload all css');
-      reloadAll();
+    if (noDocument) {
+        return noop;
     }
-  }
 
-  return debounce(update, 10);
+    getScriptSrc = getCurrentScriptUrl(moduleId);
+
+    function update() {
+        var src = getScriptSrc(options.fileMap);
+        var reloaded = reloadStyle(src);
+        if (reloaded && !options.reloadAll) {
+            console.log('[HMR] css reload %s', src.join(' '));
+        } else {
+            console.log('[HMR] Reload all css');
+            reloadAll();
+        }
+    }
+
+    return debounce(update, 10);
 };
--- css-hot-loader/loader.js
+++ css-hot-loader/loader.js
@@ -1,18 +1,17 @@
 const path = require('path');
 const loaderUtils = require('loader-utils');
 const defaultOptions = {
-  fileMap: '{fileName}',
+    fileMap: '{fileName}',
 };
-module.exports = function(content) {
-  this.cacheable();
-  const options = Object.assign(
-    {},
-    defaultOptions,
-    loaderUtils.getOptions(this)
-  );
+module.exports = function (content) {
+    this.cacheable();
+    const options = Object.assign({},
+        defaultOptions,
+        loaderUtils.getOptions(this)
+    );
 
-  const accept = options.cssModule ? '' : 'module.hot.accept(undefined, cssReload);';
-  return content + `
+    const accept = options.cssModule ? '' : 'module.hot.accept(undefined, cssReload);';
+    return content + `
     if(module.hot) {
       // ${Date.now()}
       var cssReload = require(${loaderUtils.stringifyRequest(this, '!' + path.join(__dirname, 'hotModuleReplacement.js'))})(module.id, ${JSON.stringify(options)});
