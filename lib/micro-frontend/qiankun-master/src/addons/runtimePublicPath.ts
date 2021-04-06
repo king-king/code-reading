dc7712f0fc0f7fe185ae1b0c94727e2c52b6feaa
wangqun6 <wangqun28>
2021-04-06 11:17:48 +0800

    formate

--- lib/micro-frontend/qiankun-master/src/addons/runtimePublicPath.ts
+++ lib/micro-frontend/qiankun-master/src/addons/runtimePublicPath.ts
@@ -7,31 +7,31 @@ import type { FrameworkLifeCycles } from '../interfaces';
 const rawPublicPath = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__;
 
 export default function getAddOn(global: Window, publicPath = '/'): FrameworkLifeCycles<any> {
-  let hasMountedOnce = false;
+    let hasMountedOnce = false;
 
-  return {
-    async beforeLoad() {
-      // eslint-disable-next-line no-param-reassign
-      global.__INJECTED_PUBLIC_PATH_BY_QIANKUN__ = publicPath;
-    },
+    return {
+        async beforeLoad() {
+            // eslint-disable-next-line no-param-reassign
+            global.__INJECTED_PUBLIC_PATH_BY_QIANKUN__ = publicPath;
+        },
 
-    async beforeMount() {
-      if (hasMountedOnce) {
-        // eslint-disable-next-line no-param-reassign
-        global.__INJECTED_PUBLIC_PATH_BY_QIANKUN__ = publicPath;
-      }
-    },
+        async beforeMount() {
+            if (hasMountedOnce) {
+                // eslint-disable-next-line no-param-reassign
+                global.__INJECTED_PUBLIC_PATH_BY_QIANKUN__ = publicPath;
+            }
+        },
 
-    async beforeUnmount() {
-      if (rawPublicPath === undefined) {
-        // eslint-disable-next-line no-param-reassign
-        delete global.__INJECTED_PUBLIC_PATH_BY_QIANKUN__;
-      } else {
-        // eslint-disable-next-line no-param-reassign
-        global.__INJECTED_PUBLIC_PATH_BY_QIANKUN__ = rawPublicPath;
-      }
+        async beforeUnmount() {
+            if (rawPublicPath === undefined) {
+                // eslint-disable-next-line no-param-reassign
+                delete global.__INJECTED_PUBLIC_PATH_BY_QIANKUN__;
+            } else {
+                // eslint-disable-next-line no-param-reassign
+                global.__INJECTED_PUBLIC_PATH_BY_QIANKUN__ = rawPublicPath;
+            }
 
-      hasMountedOnce = true;
-    },
-  };
+            hasMountedOnce = true;
+        },
+    };
 }
