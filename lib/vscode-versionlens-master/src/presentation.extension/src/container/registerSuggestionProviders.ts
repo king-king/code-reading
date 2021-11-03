0a2150005f0b59288b5d29478683ce026a5aee1f
wangqun28 <wangqun6@jd.com>
2021-11-03 11:23:33 +0800

    change

--- lib/vscode-versionlens-master/src/presentation.extension/src/container/registerSuggestionProviders.ts
+++ lib/vscode-versionlens-master/src/presentation.extension/src/container/registerSuggestionProviders.ts
@@ -7,50 +7,50 @@ import { IProviderModule } from 'core.providers';
 import { IContainerMap } from 'presentation.extension';
 
 export async function registerSuggestionProviders(
-  providerNames: Array<string>,
-  container: AwilixContainer<IContainerMap>,
-  logger: ILogger
+    providerNames: Array<string>,
+    container: AwilixContainer<IContainerMap>,
+    logger: ILogger
 ): Promise<Array<ISuggestionProvider>> {
 
-  logger.debug('Registering providers %o', providerNames.join(', '));
+    logger.debug('Registering providers %o', providerNames.join(', '));
 
-  const results = [];
+    const results = [];
 
-  const promised = providerNames.map(
-    function (providerName: string) {
-      return import(`infrastructure.providers/${providerName}/index`)
-        .then(
-          function (module: IProviderModule) {
+    const promised = providerNames.map(
+        function (providerName: string) {
+            return import(`infrastructure.providers/${providerName}/index`)
+                .then(
+                    function (module: IProviderModule) {
 
-            logger.debug('Activating container scope for %s', providerName);
+                        logger.debug('Activating container scope for %s', providerName);
 
-            // create a container scope for the provider
-            const scopeContainer = container.createScope();
-            const provider = module.configureContainer(scopeContainer);
+                        // create a container scope for the provider
+                        const scopeContainer = container.createScope();
+                        const provider = module.configureContainer(scopeContainer);
 
-            // register the provider
-            logger.debug(
-              "Registered provider for %s:\t file pattern: %s\t caching: %s minutes\t strict ssl: %s",
-              providerName,
-              provider.config.fileMatcher.pattern,
-              provider.config.caching.duration,
-              provider.config.http.strictSSL,
-            );
+                        // register the provider
+                        logger.debug(
+                            "Registered provider for %s:\t file pattern: %s\t caching: %s minutes\t strict ssl: %s",
+                            providerName,
+                            provider.config.fileMatcher.pattern,
+                            provider.config.caching.duration,
+                            provider.config.http.strictSSL,
+                        );
 
-            results.push(provider);
-          }
-        )
-        .catch(error => {
-          logger.error(
-            'Could not register provider %s. Reason: %O',
-            providerName,
-            error,
-          );
-        });
-    }
-  );
+                        results.push(provider);
+                    }
+                )
+                .catch(error => {
+                    logger.error(
+                        'Could not register provider %s. Reason: %O',
+                        providerName,
+                        error,
+                    );
+                });
+        }
+    );
 
-  await Promise.all(promised);
+    await Promise.all(promised);
 
-  return results;
+    return results;
 }
--- lib/vscode-versionlens-master/src/presentation.extension/src/versionLensContainer.ts
+++ lib/vscode-versionlens-master/src/presentation.extension/src/versionLensContainer.ts
@@ -1,11 +1,11 @@
 import { workspace, window, ExtensionContext } from 'vscode';
 
 import {
-  createContainer,
-  asValue,
-  asFunction,
-  InjectionMode,
-  AwilixContainer
+    createContainer,
+    asValue,
+    asFunction,
+    InjectionMode,
+    AwilixContainer
 } from 'awilix';
 
 import { LoggingOptions } from 'core.logging';
@@ -15,10 +15,10 @@ import { VsCodeConfig } from 'infrastructure.configuration';
 import { createWinstonLogger, OutputChannelTransport } from 'infrastructure.logging';
 
 import {
-  VersionLensExtension,
-  registerIconCommands,
-  registerSuggestionCommands,
-  TextEditorEvents
+    VersionLensExtension,
+    registerIconCommands,
+    registerSuggestionCommands,
+    TextEditorEvents
 } from 'presentation.extension';
 
 import { IContainerMap } from './definitions/iContainerMap';
@@ -26,126 +26,126 @@ import { registerSuggestionProviders } from './container/registerSuggestionProvi
 import { registerVersionLensProviders } from './container/registerVersionLensProviders';
 
 export async function configureContainer(
-  context: ExtensionContext
+    context: ExtensionContext
 ): Promise<AwilixContainer<IContainerMap>> {
 
-  const container: AwilixContainer<IContainerMap> = createContainer({
-    injectionMode: InjectionMode.CLASSIC
-  });
+    const container: AwilixContainer<IContainerMap> = createContainer({
+        injectionMode: InjectionMode.CLASSIC
+    });
 
-  const containerMap = {
+    const containerMap = {
 
-    extensionName: asValue(VersionLensExtension.extensionName),
+        extensionName: asValue(VersionLensExtension.extensionName),
 
-    // maps to the vscode configuration
-    rootConfig: asFunction(
-      extensionName => new VsCodeConfig(workspace, extensionName.toLowerCase())
-    ).singleton(),
+        // maps to the vscode configuration
+        rootConfig: asFunction(
+            extensionName => new VsCodeConfig(workspace, extensionName.toLowerCase())
+        ).singleton(),
 
-    // logging options
-    loggingOptions: asFunction(
-      rootConfig => new LoggingOptions(rootConfig, 'logging')
-    ).singleton(),
+        // logging options
+        loggingOptions: asFunction(
+            rootConfig => new LoggingOptions(rootConfig, 'logging')
+        ).singleton(),
 
-    httpOptions: asFunction(
-      rootConfig => new HttpOptions(rootConfig, 'http')
-    ).singleton(),
+        httpOptions: asFunction(
+            rootConfig => new HttpOptions(rootConfig, 'http')
+        ).singleton(),
 
-    cachingOptions: asFunction(
-      rootConfig => new CachingOptions(rootConfig, 'caching')
-    ).singleton(),
+        cachingOptions: asFunction(
+            rootConfig => new CachingOptions(rootConfig, 'caching')
+        ).singleton(),
 
-    // logging
-    outputChannel: asFunction(
-      extensionName => window.createOutputChannel(extensionName)
-    ).singleton(),
+        // logging
+        outputChannel: asFunction(
+            extensionName => window.createOutputChannel(extensionName)
+        ).singleton(),
 
-    outputChannelTransport: asFunction(
-      (outputChannel, loggingOptions) =>
-        new OutputChannelTransport(outputChannel, loggingOptions)
-    ).singleton(),
+        outputChannelTransport: asFunction(
+            (outputChannel, loggingOptions) =>
+                new OutputChannelTransport(outputChannel, loggingOptions)
+        ).singleton(),
 
-    logger: asFunction(
-      (outputChannelTransport) =>
-        createWinstonLogger(outputChannelTransport, { namespace: 'extension' })
-    ).singleton(),
+        logger: asFunction(
+            (outputChannelTransport) =>
+                createWinstonLogger(outputChannelTransport, { namespace: 'extension' })
+        ).singleton(),
 
-    // extension
-    extension: asFunction(
-      rootConfig => new VersionLensExtension(rootConfig)
-    ).singleton(),
+        // extension
+        extension: asFunction(
+            rootConfig => new VersionLensExtension(rootConfig)
+        ).singleton(),
 
-    // commands
-    subscriptions: asValue(context.subscriptions),
+        // commands
+        subscriptions: asValue(context.subscriptions),
 
-    iconCommands: asFunction(
-      (extension, versionLensProviders, subscriptions, outputChannel, logger) =>
-        registerIconCommands(
-          extension.state,
-          versionLensProviders,
-          subscriptions,
-          outputChannel,
-          logger.child({ namespace: 'icon commands' })
+        iconCommands: asFunction(
+            (extension, versionLensProviders, subscriptions, outputChannel, logger) =>
+                registerIconCommands(
+                    extension.state,
+                    versionLensProviders,
+                    subscriptions,
+                    outputChannel,
+                    logger.child({ namespace: 'icon commands' })
+                )
+        ).singleton(),
+
+        suggestionCommands: asFunction(
+            (extension, subscriptions, logger) =>
+                registerSuggestionCommands(
+                    extension.state,
+                    subscriptions,
+                    logger.child({ namespace: 'suggestion commands' })
+                )
+        ).singleton(),
+
+        // events
+        textEditorEvents: asFunction(
+            (extension, suggestionProviders, outputChannelTransport) =>
+                new TextEditorEvents(
+                    extension.state,
+                    suggestionProviders,
+                    outputChannelTransport
+                )
+        ).singleton(),
+
+        // codelens providers
+        providerNames: asValue([
+            'composer',
+            'dotnet',
+            'dub',
+            'jspm',
+            'maven',
+            'npm',
+            'pub',
+        ]),
+
+        versionLensProviders: asFunction(
+            (extension, suggestionProviders, subscriptions, logger) =>
+                registerVersionLensProviders(
+                    extension,
+                    suggestionProviders,
+                    subscriptions,
+                    logger
+                )
         )
-    ).singleton(),
 
-    suggestionCommands: asFunction(
-      (extension, subscriptions, logger) =>
-        registerSuggestionCommands(
-          extension.state,
-          subscriptions,
-          logger.child({ namespace: 'suggestion commands' })
-        )
-    ).singleton(),
+    };
 
-    // events
-    textEditorEvents: asFunction(
-      (extension, suggestionProviders, outputChannelTransport) =>
-        new TextEditorEvents(
-          extension.state,
-          suggestionProviders,
-          outputChannelTransport
-        )
-    ).singleton(),
+    // register the map
+    container.register(containerMap);
 
-    // codelens providers
-    providerNames: asValue([
-      'composer',
-      'dotnet',
-      'dub',
-      'jspm',
-      'maven',
-      'npm',
-      'pub',
-    ]),
-
-    versionLensProviders: asFunction(
-      (extension, suggestionProviders, subscriptions, logger) =>
-        registerVersionLensProviders(
-          extension,
-          suggestionProviders,
-          subscriptions,
-          logger
-        )
+    // generate the provider registry async
+    const { logger, providerNames } = container.cradle;
+    const suggestionProviders = await registerSuggestionProviders(
+        providerNames,
+        container,
+        logger.child({ namespace: 'registry' })
     )
 
-  };
+    // add the registry in to the container
+    container.register({
+        suggestionProviders: asValue(suggestionProviders)
+    });
 
-  // register the map
-  container.register(containerMap);
-
-  // generate the provider registry async
-  const { logger, providerNames } = container.cradle;
-  const suggestionProviders = await registerSuggestionProviders(
-    providerNames,
-    container,
-    logger.child({ namespace: 'registry' })
-  )
-
-  // add the registry in to the container
-  container.register({
-    suggestionProviders: asValue(suggestionProviders)
-  });
-
-  return container;
+    return container;
 }
