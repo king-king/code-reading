ac4a5e6d990ca7dd72d85bffbd81ac0e05461daa
wangqun28 <wangqun6@jd.com>
2022-03-03 11:34:37 +0800

    change

--- lib/antv/G2Plot-master/src/plots/box/adaptor.ts
+++ lib/antv/G2Plot-master/src/plots/box/adaptor.ts
@@ -14,75 +14,75 @@ import { transformData } from './utils';
  * @param params
  */
 function field(params: Params<BoxOptions>): Params<BoxOptions> {
-  const { chart, options } = params;
-  const { xField, yField, groupField, color, tooltip, boxStyle } = options;
+    const { chart, options } = params;
+    const { xField, yField, groupField, color, tooltip, boxStyle } = options;
 
-  chart.data(transformData(options.data, yField));
+    chart.data(transformData(options.data, yField));
 
-  const yFieldName = isArray(yField) ? BOX_RANGE : yField;
-  const rawFields = yField ? (isArray(yField) ? yField : [yField]) : [];
+    const yFieldName = isArray(yField) ? BOX_RANGE : yField;
+    const rawFields = yField ? (isArray(yField) ? yField : [yField]) : [];
 
-  let tooltipOptions = tooltip;
-  if (tooltipOptions !== false) {
-    tooltipOptions = deepAssign({}, { fields: isArray(yField) ? yField : [] }, tooltipOptions);
-  }
+    let tooltipOptions = tooltip;
+    if (tooltipOptions !== false) {
+        tooltipOptions = deepAssign({}, { fields: isArray(yField) ? yField : [] }, tooltipOptions);
+    }
 
-  const { ext } = schema(
-    deepAssign({}, params, {
-      options: {
-        xField,
-        yField: yFieldName,
-        seriesField: groupField,
-        tooltip: tooltipOptions,
-        rawFields,
-        // 只有异常点视图展示 label
-        label: false,
-        schema: {
-          shape: 'box',
-          color,
-          style: boxStyle,
-        },
-      },
-    })
-  );
+    const { ext } = schema(
+        deepAssign({}, params, {
+            options: {
+                xField,
+                yField: yFieldName,
+                seriesField: groupField,
+                tooltip: tooltipOptions,
+                rawFields,
+                // 只有异常点视图展示 label
+                label: false,
+                schema: {
+                    shape: 'box',
+                    color,
+                    style: boxStyle,
+                },
+            },
+        })
+    );
 
-  if (groupField) {
-    ext.geometry.adjust('dodge');
-  }
+    if (groupField) {
+        ext.geometry.adjust('dodge');
+    }
 
-  return params;
+    return params;
 }
 
 /**
  * 创建异常点 view
  */
 function outliersPoint(params: Params<BoxOptions>): Params<BoxOptions> {
-  const { chart, options } = params;
-  const { xField, data, outliersField, outliersStyle, padding, label } = options;
+    const { chart, options } = params;
+    const { xField, data, outliersField, outliersStyle, padding, label } = options;
 
-  if (!outliersField) return params;
+    if (!outliersField) return params;
 
-  const outliersView = chart.createView({ padding, id: OUTLIERS_VIEW_ID });
-  const outliersViewData = data.reduce((ret, datum) => {
-    const outliersData = datum[outliersField];
-    outliersData.forEach((d) => ret.push({ ...datum, [outliersField]: d }));
-    return ret;
-  }, []) as Types.Datum[];
+    const outliersView = chart.createView({ padding, id: OUTLIERS_VIEW_ID });
+    const outliersViewData = data.reduce((ret, datum) => {
+        const outliersData = datum[outliersField];
+        outliersData.forEach((d) => ret.push({ ...datum, [outliersField]: d }));
+        return ret;
+    }, []) as Types.Datum[];
 
-  outliersView.data(outliersViewData);
-  point({
-    chart: outliersView,
-    options: {
-      xField,
-      yField: outliersField,
-      point: { shape: 'circle', style: outliersStyle },
-      label,
-    },
-  });
+    outliersView.data(outliersViewData);
+    point({
+        chart: outliersView,
+        options: {
+            xField,
+            yField: outliersField,
+            point: { shape: 'circle', style: outliersStyle },
+            label,
+        },
+    });
 
-  outliersView.axis(false);
+    outliersView.axis(false);
 
-  return params;
+    return params;
 }
 
 /**
@@ -90,29 +90,29 @@ function outliersPoint(params: Params<BoxOptions>): Params<BoxOptions> {
  * @param params
  */
 function meta(params: Params<BoxOptions>): Params<BoxOptions> {
-  const { chart, options } = params;
-  const { meta, xAxis, yAxis, xField, yField, outliersField } = options;
-  const yFieldName = Array.isArray(yField) ? BOX_RANGE : yField;
+    const { chart, options } = params;
+    const { meta, xAxis, yAxis, xField, yField, outliersField } = options;
+    const yFieldName = Array.isArray(yField) ? BOX_RANGE : yField;
 
-  let baseMeta = {};
+    let baseMeta = {};
 
-  // make yField and outliersField share y mate
-  if (outliersField) {
-    const syncName = BOX_SYNC_NAME;
-    baseMeta = {
-      [outliersField]: { sync: syncName, nice: true },
-      [yFieldName]: { sync: syncName, nice: true },
-    };
-  }
+    // make yField and outliersField share y mate
+    if (outliersField) {
+        const syncName = BOX_SYNC_NAME;
+        baseMeta = {
+            [outliersField]: { sync: syncName, nice: true },
+            [yFieldName]: { sync: syncName, nice: true },
+        };
+    }
 
-  const scales = deepAssign(baseMeta, meta, {
-    [xField]: pick(xAxis, AXIS_META_CONFIG_KEYS),
-    [yFieldName]: pick(yAxis, AXIS_META_CONFIG_KEYS),
-  });
+    const scales = deepAssign(baseMeta, meta, {
+        [xField]: pick(xAxis, AXIS_META_CONFIG_KEYS),
+        [yFieldName]: pick(yAxis, AXIS_META_CONFIG_KEYS),
+    });
 
-  chart.scale(scales);
+    chart.scale(scales);
 
-  return params;
+    return params;
 }
 
 /**
@@ -120,24 +120,24 @@ function meta(params: Params<BoxOptions>): Params<BoxOptions> {
  * @param params
  */
 function axis(params: Params<BoxOptions>): Params<BoxOptions> {
-  const { chart, options } = params;
-  const { xAxis, yAxis, xField, yField } = options;
-  const yFieldName = Array.isArray(yField) ? BOX_RANGE : yField;
+    const { chart, options } = params;
+    const { xAxis, yAxis, xField, yField } = options;
+    const yFieldName = Array.isArray(yField) ? BOX_RANGE : yField;
 
-  // 为 false 则是不显示轴
-  if (xAxis === false) {
-    chart.axis(xField, false);
-  } else {
-    chart.axis(xField, xAxis);
-  }
+    // 为 false 则是不显示轴
+    if (xAxis === false) {
+        chart.axis(xField, false);
+    } else {
+        chart.axis(xField, xAxis);
+    }
 
-  if (yAxis === false) {
-    chart.axis(BOX_RANGE, false);
-  } else {
-    chart.axis(yFieldName, yAxis);
-  }
+    if (yAxis === false) {
+        chart.axis(BOX_RANGE, false);
+    } else {
+        chart.axis(yFieldName, yAxis);
+    }
 
-  return params;
+    return params;
 }
 
 /**
@@ -145,21 +145,21 @@ function axis(params: Params<BoxOptions>): Params<BoxOptions> {
  * @param params
  */
 export function legend(params: Params<BoxOptions>): Params<BoxOptions> {
-  const { chart, options } = params;
-  const { legend, groupField } = options;
+    const { chart, options } = params;
+    const { legend, groupField } = options;
 
-  if (groupField) {
-    if (legend) {
-      chart.legend(groupField, legend);
+    if (groupField) {
+        if (legend) {
+            chart.legend(groupField, legend);
+        } else {
+            // Grouped Box Chart default has legend, and it's position is `bottom`
+            chart.legend(groupField, { position: 'bottom' });
+        }
     } else {
-      // Grouped Box Chart default has legend, and it's position is `bottom`
-      chart.legend(groupField, { position: 'bottom' });
+        chart.legend(false);
     }
-  } else {
-    chart.legend(false);
-  }
 
-  return params;
+    return params;
 }
 
 /**
@@ -167,5 +167,5 @@ export function legend(params: Params<BoxOptions>): Params<BoxOptions> {
  * @param params
  */
 export function adaptor(params: Params<BoxOptions>) {
-  return flow(field, outliersPoint, meta, axis, legend, tooltip, interaction, animation, theme)(params);
+    return flow(field, outliersPoint, meta, axis, legend, tooltip, interaction, animation, theme)(params);
 }
