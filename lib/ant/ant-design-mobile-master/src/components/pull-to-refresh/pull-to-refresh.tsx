dfb004bfc111c60b54b20e5621cabd211cc2874d
wangqun28 <wangqun6@jd.com>
2022-03-07 12:07:10 +0800

    change

--- lib/ant/ant-design-mobile-master/src/components/pull-to-refresh/pull-to-refresh.tsx
+++ lib/ant/ant-design-mobile-master/src/components/pull-to-refresh/pull-to-refresh.tsx
@@ -13,155 +13,155 @@ const classPrefix = `adm-pull-to-refresh`
 export type PullStatus = 'pulling' | 'canRelease' | 'refreshing' | 'complete'
 
 export type PullToRefreshProps = {
-  onRefresh?: () => Promise<any>
-  pullingText?: ReactNode
-  canReleaseText?: ReactNode
-  refreshingText?: ReactNode
-  completeText?: ReactNode
-  completeDelay?: number
-  headHeight?: number
-  threshold?: number
-  disabled?: boolean
-  renderText?: (status: PullStatus) => ReactNode
+    onRefresh?: () => Promise<any>
+    pullingText?: ReactNode
+    canReleaseText?: ReactNode
+    refreshingText?: ReactNode
+    completeText?: ReactNode
+    completeDelay?: number
+    headHeight?: number
+    threshold?: number
+    disabled?: boolean
+    renderText?: (status: PullStatus) => ReactNode
 }
 
 export const defaultProps = {
-  pullingText: '下拉刷新',
-  canReleaseText: '释放立即刷新',
-  refreshingText: '加载中……',
-  completeText: '刷新成功',
-  completeDelay: 500,
-  disabled: false,
-  onRefresh: () => {},
+    pullingText: '下拉刷新',
+    canReleaseText: '释放立即刷新',
+    refreshingText: '加载中……',
+    completeText: '刷新成功',
+    completeDelay: 500,
+    disabled: false,
+    onRefresh: () => { },
 }
 
 export const PullToRefresh: FC<PullToRefreshProps> = p => {
-  const props = mergeProps(defaultProps, p)
-  const headHeight = props.headHeight ?? convertPx(40)
-  const threshold = props.threshold ?? convertPx(60)
+    const props = mergeProps(defaultProps, p)
+    const headHeight = props.headHeight ?? convertPx(40)
+    const threshold = props.threshold ?? convertPx(60)
 
-  const [status, setStatus] = useState<PullStatus>('pulling')
+    const [status, setStatus] = useState<PullStatus>('pulling')
 
-  const [springStyles, api] = useSpring(() => ({
-    from: { height: 0 },
-    config: {
-      tension: 300,
-      friction: 30,
-      clamp: true,
-    },
-  }))
-
-  const elementRef = useRef<HTMLDivElement>(null)
-
-  const pullingRef = useRef(false)
-
-  //防止下拉时抖动
-  useEffect(() => {
-    elementRef.current?.addEventListener('touchmove', () => {})
-  }, [])
-
-  async function doRefresh() {
-    api.start({ height: headHeight })
-    setStatus('refreshing')
-    try {
-      await props.onRefresh()
-      setStatus('complete')
-    } catch (e) {
-      api.start({
-        to: async next => {
-          await next({ height: 0 })
-          setStatus('pulling')
+    const [springStyles, api] = useSpring(() => ({
+        from: { height: 0 },
+        config: {
+            tension: 300,
+            friction: 30,
+            clamp: true,
         },
-      })
+    }))
 
-      throw e
-    }
-    if (props.completeDelay > 0) {
-      await sleep(props.completeDelay)
-    }
-    api.start({
-      to: async next => {
-        await next({ height: 0 })
-        setStatus('pulling')
-      },
-    })
-  }
+    const elementRef = useRef<HTMLDivElement>(null)
 
-  useDrag(
-    state => {
-      if (status === 'refreshing' || status === 'complete') return
+    const pullingRef = useRef(false)
 
-      const { event } = state
+    //防止下拉时抖动
+    useEffect(() => {
+        elementRef.current?.addEventListener('touchmove', () => { })
+    }, [])
 
-      if (state.last) {
-        pullingRef.current = false
-        if (status === 'canRelease') {
-          doRefresh()
-        } else {
-          api.start({ height: 0 })
+    async function doRefresh() {
+        api.start({ height: headHeight })
+        setStatus('refreshing')
+        try {
+            await props.onRefresh()
+            setStatus('complete')
+        } catch (e) {
+            api.start({
+                to: async next => {
+                    await next({ height: 0 })
+                    setStatus('pulling')
+                },
+            })
+
+            throw e
         }
-        return
-      }
-
-      const [, y] = state.movement
-      if (state.first) {
-        const element = elementRef.current
-        if (!element) return
-        const scrollParent = getScrollParent(element)
-        if (!scrollParent) return
-        const top =
-          'scrollTop' in scrollParent
-            ? scrollParent.scrollTop
-            : scrollParent.scrollY
-        if (top <= 0 && y > 0) {
-          pullingRef.current = true
+        if (props.completeDelay > 0) {
+            await sleep(props.completeDelay)
         }
-      }
-
-      if (!pullingRef.current) return
-
-      if (event.cancelable) {
-        event.preventDefault()
-      }
-      event.stopPropagation()
-      const height = Math.max(
-        rubberbandIfOutOfBounds(y, 0, 0, headHeight * 5, 0.5),
-        0
-      )
-      api.start({ height })
-      setStatus(height > threshold ? 'canRelease' : 'pulling')
-    },
-    {
-      pointer: { touch: true },
-      axis: 'y',
-      target: elementRef,
-      enabled: !props.disabled,
-      eventOptions: supportsPassive ? { passive: false } : false,
-    }
-  )
-
-  const renderStatusText = () => {
-    if (props.renderText) {
-      return props.renderText?.(status)
+        api.start({
+            to: async next => {
+                await next({ height: 0 })
+                setStatus('pulling')
+            },
+        })
     }
 
-    if (status === 'pulling') return props.pullingText
-    if (status === 'canRelease') return props.canReleaseText
-    if (status === 'refreshing') return props.refreshingText
-    if (status === 'complete') return props.completeText
-  }
+    useDrag(
+        state => {
+            if (status === 'refreshing' || status === 'complete') return
 
-  return (
-    <animated.div ref={elementRef} className={classPrefix}>
-      <animated.div style={springStyles} className={`${classPrefix}-head`}>
-        <div
-          className={`${classPrefix}-head-content`}
-          style={{ height: headHeight }}
-        >
-          {renderStatusText()}
-        </div>
-      </animated.div>
-      <div className={`${classPrefix}-content`}>{props.children}</div>
-    </animated.div>
-  )
+            const { event } = state
+
+            if (state.last) {
+                pullingRef.current = false
+                if (status === 'canRelease') {
+                    doRefresh()
+                } else {
+                    api.start({ height: 0 })
+                }
+                return
+            }
+
+            const [, y] = state.movement
+            if (state.first) {
+                const element = elementRef.current
+                if (!element) return
+                const scrollParent = getScrollParent(element)
+                if (!scrollParent) return
+                const top =
+                    'scrollTop' in scrollParent
+                        ? scrollParent.scrollTop
+                        : scrollParent.scrollY
+                if (top <= 0 && y > 0) {
+                    pullingRef.current = true
+                }
+            }
+
+            if (!pullingRef.current) return
+
+            if (event.cancelable) {
+                event.preventDefault()
+            }
+            event.stopPropagation()
+            const height = Math.max(
+                rubberbandIfOutOfBounds(y, 0, 0, headHeight * 5, 0.5),
+                0
+            )
+            api.start({ height })
+            setStatus(height > threshold ? 'canRelease' : 'pulling')
+        },
+        {
+            pointer: { touch: true },
+            axis: 'y',
+            target: elementRef,
+            enabled: !props.disabled,
+            eventOptions: supportsPassive ? { passive: false } : false,
+        }
+    )
+
+    const renderStatusText = () => {
+        if (props.renderText) {
+            return props.renderText?.(status)
+        }
+
+        if (status === 'pulling') return props.pullingText
+        if (status === 'canRelease') return props.canReleaseText
+        if (status === 'refreshing') return props.refreshingText
+        if (status === 'complete') return props.completeText
+    }
+
+    return (
+        <animated.div ref={elementRef} className={classPrefix}>
+            <animated.div style={springStyles} className={`${classPrefix}-head`}>
+                <div
+                    className={`${classPrefix}-head-content`}
+                    style={{ height: headHeight }}
+                >
+                    {renderStatusText()}
+                </div>
+            </animated.div>
+            <div className={`${classPrefix}-content`}>{props.children}</div>
+        </animated.div>
+    )
 }
