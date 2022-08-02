0a502ca7f97cc4bbe67caade864aa87c9dbcf219
wangqun28 <wangqun6@jd.com>
2022-08-02 21:37:20 +0800

    formate

--- lib/lowcode-engine-1.0.13/modules/code-generator/src/standalone-loader.ts
+++ lib/lowcode-engine-1.0.13/modules/code-generator/src/standalone-loader.ts
@@ -15,119 +15,119 @@ export const DEFAULT_TIMEOUT_IN_MS = 60 * 1000;
 const workerJsCache = new Map<string, { content: string; url: string }>();
 
 export async function init({
-  workerJsUrl = DEFAULT_WORKER_JS,
+    workerJsUrl = DEFAULT_WORKER_JS,
 }: {
-  workerJsUrl?: string;
+    workerJsUrl?: string;
 } = {}) {
-  await loadWorkerJs(workerJsUrl);
+    await loadWorkerJs(workerJsUrl);
 }
 
 export type Result = ResultDir | FlattenFile[];
 
 export async function generateCode(options: {
-  solution: 'icejs' | 'rax';
-  schema: ProjectSchema;
-  flattenResult?: boolean;
-  workerJsUrl?: string;
-  timeoutInMs?: number;
+    solution: 'icejs' | 'rax';
+    schema: ProjectSchema;
+    flattenResult?: boolean;
+    workerJsUrl?: string;
+    timeoutInMs?: number;
 }): Promise<Result> {
-  if (typeof self !== 'object') {
-    throw new Error('self is not defined');
-  }
+    if (typeof self !== 'object') {
+        throw new Error('self is not defined');
+    }
 
-  if (typeof Worker !== 'function') {
-    throw new Error('Worker is not supported');
-  }
+    if (typeof Worker !== 'function') {
+        throw new Error('Worker is not supported');
+    }
 
-  const workerJsUrl = options.workerJsUrl || DEFAULT_WORKER_JS;
+    const workerJsUrl = options.workerJsUrl || DEFAULT_WORKER_JS;
 
-  const workerJs = await loadWorkerJs(workerJsUrl);
+    const workerJs = await loadWorkerJs(workerJsUrl);
 
-  const worker = new Worker(workerJs.url, {
-    type: 'classic',
-    credentials: 'omit',
-  });
-
-  return new Promise((resolve, reject) => {
-    const timer = setTimeout(() => {
-      reject(new Error('timeout'));
-      worker.terminate();
-    }, options.timeoutInMs || DEFAULT_TIMEOUT_IN_MS);
-
-    worker.onmessage = (event: any) => {
-      const msg = event.data;
-      switch (msg.type) {
-        case 'ready':
-          print('worker is ready.');
-          break;
-
-        case 'run:begin':
-          print('worker is running...');
-          break;
-        case 'run:end':
-          print('worker is done.');
-          resolve(msg.result);
-          clearTimeout(timer);
-          worker.terminate();
-          break;
-        case 'run:error':
-          printErr(`worker error: ${msg.errorMsg}`);
-          clearTimeout(timer);
-          reject(new Error(msg.errorMsg || 'unknown error'));
-          worker.terminate();
-          break;
-        default:
-          print('got unknown msg: %o', msg);
-          break;
-      }
-    };
-
-    worker.onerror = (err: any) => {
-      printErr('worker error: %o', err);
-      clearTimeout(timer);
-      reject(err);
-      worker.terminate();
-    };
-
-    worker.postMessage({
-      type: 'run',
-      solution: options.solution,
-      schema: options.schema,
-      flattenResult: options.flattenResult,
+    const worker = new Worker(workerJs.url, {
+        type: 'classic',
+        credentials: 'omit',
     });
-  });
+
+    return new Promise((resolve, reject) => {
+        const timer = setTimeout(() => {
+            reject(new Error('timeout'));
+            worker.terminate();
+        }, options.timeoutInMs || DEFAULT_TIMEOUT_IN_MS);
+
+        worker.onmessage = (event: any) => {
+            const msg = event.data;
+            switch (msg.type) {
+                case 'ready':
+                    print('worker is ready.');
+                    break;
+
+                case 'run:begin':
+                    print('worker is running...');
+                    break;
+                case 'run:end':
+                    print('worker is done.');
+                    resolve(msg.result);
+                    clearTimeout(timer);
+                    worker.terminate();
+                    break;
+                case 'run:error':
+                    printErr(`worker error: ${msg.errorMsg}`);
+                    clearTimeout(timer);
+                    reject(new Error(msg.errorMsg || 'unknown error'));
+                    worker.terminate();
+                    break;
+                default:
+                    print('got unknown msg: %o', msg);
+                    break;
+            }
+        };
+
+        worker.onerror = (err: any) => {
+            printErr('worker error: %o', err);
+            clearTimeout(timer);
+            reject(err);
+            worker.terminate();
+        };
+
+        worker.postMessage({
+            type: 'run',
+            solution: options.solution,
+            schema: options.schema,
+            flattenResult: options.flattenResult,
+        });
+    });
 }
 
 async function loadWorkerJs(workerJsUrl: string) {
-  const cached = workerJsCache.get(workerJsUrl);
-  if (cached) {
-    return cached;
-  }
+    const cached = workerJsCache.get(workerJsUrl);
+    if (cached) {
+        return cached;
+    }
 
-  const workerJsContent = await fetch(workerJsUrl)
-    .then((res) => res.text())
-    .catch((err) => {
-      throw new Error(`Failed to fetch worker js: ${err}`);
-    });
+    const workerJsContent = await fetch(workerJsUrl)
+        .then((res) => res.text())
+        .catch((err) => {
+            throw new Error(`Failed to fetch worker js: ${err}`);
+        });
 
-  const workerJs = {
-    content: workerJsContent,
-    url: self.URL.createObjectURL(
-      new self.Blob([workerJsContent], { type: 'application/javascript' }),
-    ),
-  };
+    const workerJs = {
+        content: workerJsContent,
+        url: self.URL.createObjectURL(
+            new self.Blob([workerJsContent], { type: 'application/javascript' }),
+        ),
+    };
 
-  workerJsCache.set(workerJsUrl, workerJs);
+    workerJsCache.set(workerJsUrl, workerJs);
 
-  return workerJs;
+    return workerJs;
 }
 
 function print(msg: string, ...args: unknown[]) {
-  // eslint-disable-next-line no-console
-  console.debug(`[code-generator/loader]: ${msg}`, ...args);
+    // eslint-disable-next-line no-console
+    console.debug(`[code-generator/loader]: ${msg}`, ...args);
 }
 
 function printErr(msg: string, ...args: unknown[]) {
-  // eslint-disable-next-line no-console
-  console.debug(`[code-generator/loader]: %c${msg}`, 'color:red', ...args);
+    // eslint-disable-next-line no-console
+    console.debug(`[code-generator/loader]: %c${msg}`, 'color:red', ...args);
 }
